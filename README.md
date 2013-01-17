198245df47d1efe38bea32c34d126cdf878a7016
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2013-01-17 16:12:05 -0600

    Adding zoomer button control for a button that zooms to a place.

--- README.md
+++ README.md
@@ -12,9 +12,14 @@ Include the following in your page (with the regular Leaflet scripts).
  
 ## Implements
 
- - Fullscreen control.  Based from [this pull request](https://github.com/CloudMade/Leaflet/pull/603).  To use with Leaflet, simply include in your JS: ```map.addControl(new L.Control.FullScreen);```
+ - Fullscreen control.  Based from [this pull request](https://github.com/CloudMade/Leaflet/pull/603).
+     - Simply include in your JS: ```map.addControl(new L.Control.Fullscreen());```
+ 
+ - Zoomer Button control.  This is simply a button that will zoom/pan into a place.
+     - Simply include in your JS: ```map.addControl(new L.Control.ZoomerButton());```
+     - Options: ```place, zoom, title```
  
 ## Building
 
-1. Install: ```npm install -g jake; npm install jshint; npm install uglify-js;```
+1. Install: ```npm install -g jake; npm install jshint; npm install uglify-js@1.x;```
 1. Run form repository root: ```jake```
--- dist/leaflet-addons.js
+++ dist/leaflet-addons.js
@@ -83,3 +83,45 @@ L.Map.include({
 
 })(L, window);
 
+/**
+ * Control that will zoom to a specific place.
+ */
+(function(L, w, undefined) {
+
+L.Control.ZoomerButton = L.Control.extend({
+  options: {
+    position: 'topleft',
+    place: [0, 0],
+    zoom: 4,
+    title: 'Zoom into a place'
+  },
+
+  onAdd: function (map) {
+    this._map = map;
+    var className = 'leaflet-control-zoomer-button';
+    var container = L.DomUtil.create('div', className);
+
+    this._createButton(this.options.title, className + '-button', container, this._zoomMap, this);
+    
+    return container;
+  },
+  
+  _createButton: function (title, className, container, fn, context) {
+    var link = L.DomUtil.create('a', className, container);
+    link.href = '#';
+    link.title = title;
+    
+    L.DomEvent.addListener(link, 'click', L.DomEvent.stopPropagation);
+    L.DomEvent.addListener(link, 'click', L.DomEvent.preventDefault);
+    L.DomEvent.addListener(link, 'click', fn, context);
+    
+    return link;
+  },
+
+  _zoomMap: function (event) {
+    this._map.setView(this.options.place, this.options.zoom);
+  }
+});
+
+})(L, window);
+
